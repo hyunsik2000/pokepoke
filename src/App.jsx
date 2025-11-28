a63780b3a54735db725d76f1e641222e896794cd
hyunsik2000 <ryuhsik2000@naver.com>
2025-11-28 11:05:28 +0900

    feat: 디렉토리 구조 + 라우팅 틀 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+import SearchPage from "./pages/SearchPage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path={"/"} element={<MainPage />} />
+          <Route path={"/detail/:pokemonId"} element={<DetailPage />} />
+          <Route path={"/search"} element={<SearchPage />} />
+          <Route path={"/favorties"} element={<FavoritesPage />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
